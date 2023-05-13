198ce44c7d4c0d4d93f0471f03a6fd2a7c74ee64
Zerubbabel14 <davidzerubbabel754@gmail.com>
2023-05-13 16:17:58 +0530

    Mini Project

---
+++ BCK/registration.js
@@ -0,0 +1,3 @@
+function fun2(){
+    alert("I have accepted to register as Voluntary Blood Donor. I accept that my details will be available to the Blood Banks and blood banks may contact in case of emergency.");
+}
